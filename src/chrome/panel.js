dd4d36aac148a6865907415d60e8b498d9d27c69
Stefan Rimaila <stefan@rimaila.fi>
2015-11-14 15:20:45 +0200

    Network request handling made saner

--- src/chrome/panel.js
+++ src/chrome/panel.js
@@ -7,6 +7,7 @@
  */
 import T from 'immutable';
 import kancolleApi from '../config/kancolleApi';
+import addonEvents from '../enums/addonEvents';
 
 const port = chrome.runtime.connect({ name: kancolleApi.channelName });
 
@@ -14,5 +15,37 @@ chrome.devtools.network.onRequestFinished.addListener((networkRequest) => {
   const { response, request } = networkRequest;
   const acceptedContent = T.List.of('text/javascript', 'text/html', 'text/plain');
 
+  let contentType = null;
 
+  // @fixme Please make me saner
+  response.headers.forEach((it) => {
+    if (it.name.toLowerCase() === 'content-type' && acceptedContent.includes(it.value)) {
+      contentType = it.value;
+    }
+  });
+
+  // @fixme No, pls, help
+  if (contentType) {
+    networkRequest.getContent((requestContent) => {
+      if (requestContent.includes(kancolleApi.panel.apiDataPrefix)) {
+        let content = requestContent.substring(kancolleApi.panel.apiDataPrefix.length);
+        let path = request.url.replace(/.*\/kcsapi/, '');
+        let apiJsonData = null;
+
+        try {
+          apiJsonData = JSON.parse(content)['api_data'];
+        }
+        catch (e) {
+          port.postMessage({ event: addonEvents.REQUEST_CONTENT_PARSE_ERROR });
+        }
+
+        let apiRequestObject = T.Map({
+          event: addonEvents.API_DATA_RECEIVED,
+          content, response, request, path, apiJsonData
+        });
+
+        port.postMessage(apiRequestObject);
+      }
+    });
+  }
 });
---
+++ src/enums/addonEvents.js
@@ -0,0 +1,14 @@
+/**
+ *
+ */
+import keymirror from 'keymirror';
+
+/**
+ * @type {object}
+ * @property {string} API_DATA_RECEIVED
+ * @property {string} REQUEST_CONTENT_PARSE_ERROR
+ */
+export default keymirror({
+  API_DATA_RECEIVED: null,
+  REQUEST_CONTENT_PARSE_ERROR: null
+})

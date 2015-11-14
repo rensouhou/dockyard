cf2f076fe5a1050b75cca42faf110bdf64f600d7
Stefan Rimaila <stefan@rimaila.fi>
2015-11-14 14:34:31 +0200

    Added KCApi data

---
+++ src/config/kancolleApi.js
@@ -0,0 +1,15 @@
+/**
+ * @overview
+ *
+ * @since 0.1.0
+ * @author Stefan Rimaila <stefan@rimaila.fi>
+ * @module config/kancolleApi
+ */
+export default {
+  channelName: 'Dockyard',
+  panel: {
+    rootEventName: 'kancolledata',
+    rootEventNode: 'body',
+    apiDataPrefix: 'svdata='
+  }
+};

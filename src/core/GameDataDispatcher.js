9dc084b0641621ad594939e68b7912fa1d9280dd
Stefan Rimaila <stefan@rimaila.fi>
2015-12-11 23:26:44 +0200

    Added dispatcher

---
+++ src/core/GameDataDispatcher.js
@@ -0,0 +1,15 @@
+/**
+ * @overview
+ *
+ * @since 0.0.0
+ * @author Anon
+ * @module
+ */
+import { Dispatcher } from 'flux';
+
+/**
+ * @type {Dockyard.GameDataDispatcher}
+ */
+const dispatcher = new Dispatcher();
+
+export default dispatcher;

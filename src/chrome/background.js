36e3782973ebfa412c7a0c1d5c64aea9b0c45e3b
Stefan Rimaila <stefan@rimaila.fi>
2015-12-16 09:28:59 +0200

    Removing firebase dumping for now

--- src/chrome/background.js
+++ src/chrome/background.js
@@ -15,46 +15,47 @@ import NetworkRequestHandler from '../core/NetworkRequestHandler';
 import { GameDataHandler } from '../core/game/index';
 
 import AddonEvent from '../enums/addonEvents';
-import { firebaseRef } from '../../runtime/firebase';
 import ApiDataStore from '../core/game/stores/ApiDataStore';
 import dispatcher from '../core/GameDataDispatcher';
 
 let ApiDataHandler = new GameDataHandler();
-let apiDataStore = new ApiDataStore(dispatcher, ['UPDATE_ACTION', 'MERGE_ACTION']);
+let apiDataStore = new ApiDataStore(dispatcher);
 
-chrome.runtime.onConnect.addListener((port) => {
-  console.log('Added listener for channel %s', port.name);
+chrome.runtime.onConnect.addListener(
+  /**
+   * @param {Port} port
+   */
+  function (port) {
+    console.log('Added listener for channel %s', port.name);
 
-  port.onMessage.addListener(
-    /**
-     * @param {NetworkEventMsg} msg
-     */
-    (msg) => {
-      if (_.isArray(msg)) {
-        console.log.apply(console, ['debug =>'].concat(msg));
-      }
+    port.onMessage.addListener(
+      /**
+       * @param {Dockyard.NetworkEventMsg} msg
+       */
+      function (msg) {
+        if (_.isArray(msg)) {
+          console.log.apply(console, ['debug =>'].concat(msg));
+        }
 
-      if (msg.event && msg.event === AddonEvent.API_DATA_RECEIVED) {
-        const { chromeNetworkRequest, content } = msg;
-        const { request, response } = chromeNetworkRequest;
-        const timestamp = +(new Date());
+        if (msg.event && msg.event === AddonEvent.API_DATA_RECEIVED) {
+          const { chromeNetworkRequest, content } = msg;
+          const { request, response } = chromeNetworkRequest;
+          const timestamp = +(new Date());
 
-        let RequestHandler = new NetworkRequestHandler({ timestamp, request, response, content });
-        let resultRecord = RequestHandler.getData();
+          let RequestHandler = new NetworkRequestHandler({ timestamp, request, response, content });
+          let resultRecord = RequestHandler.getData();
 
-        // Just dump everything into Firebase
-        let r = resultRecord.toJS();
-        firebaseRef.child(r.event).push(r);
+          let r = resultRecord.toJS();
 
-        console.group('Event => %s', r.event);
-        console.info('path\t\t=> %s', r.path);
-        console.info('timestamp\t=> %s', timestamp);
-        console.info('GET\t\t\t=> %O', r.GET);
-        console.info('POST\t\t=> %O', r.POST);
-        console.groupEnd();
+          console.group('Event => %s', r.event);
+          console.info('path\t\t=> %s', r.path);
+          console.info('timestamp\t=> %s', timestamp);
+          console.info('GET\t\t\t=> %O', r.method.GET);
+          console.info('POST\t\t=> %O', r.method.POST);
+          console.groupEnd();
 
-        ApiDataHandler.handleEvent(resultRecord, dispatcher);
-      }
-    })
-});
+          ApiDataHandler.handleEvent(resultRecord, dispatcher);
+        }
+      })
+  });
 

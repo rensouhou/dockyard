94af38d75d7aed4b957632a839010a96797fa1af
Stefan Rimaila <stefan@rimaila.fi>
2016-01-03 12:52:32 +0200

    Base data handler

--- src/core/game/handlers/GetBaseData.js
+++ src/core/game/handlers/GetBaseData.js
@@ -1,17 +1,30 @@
 /**
  * @overview
+ *
+ *
  */
-import T from 'immutable';
+import R from 'ramda';
 
 import BaseHandler from './BaseHandler';
+import Action from '../constants/Actions';
+import { apiData } from '../transformers';
 
 export default class GetBaseData extends BaseHandler {
   handleState() {
     console.log('GetBaseData');
     console.log('└─ eventRecord => %O', this.eventRecord.toJS());
 
-    //let profile = T.fromJS(this.GET.api_basic);
+    /** @type {kcsapi.api.GetBaseData.GET} */
+    const GET = this.method.GET;
 
-    //this.dispatchState('BASE_DATA');
+    let profile = apiData.transformPlayerProfile(GET.api_basic);
+    let fleets = R.map(apiData.transformFleet, GET.api_deck_port);
+
+    let payload = { profile, fleets };
+
+    this.dispatchState({
+      type: Action.UPDATE_PLAYER_STATE,
+      payload
+    });
   }
 }

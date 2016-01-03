ce3a458d5c8819a3db2f67d3ae0c187cba448fc3
Stefan Rimaila <stefan@rimaila.fi>
2016-01-03 12:55:07 +0200

    Moved quest list transformations to their own transformer functions

--- src/core/game/handlers/GetQuestList.js
+++ src/core/game/handlers/GetQuestList.js
@@ -5,43 +5,33 @@
  * @author Stefan Rimaila <stefan@rimaila.fi>
  * @module src/core/game/handlers/GetQuestList
  */
-const R = require('ramda');
+import R from 'ramda';
 
 import BaseHandler from './BaseHandler';
 import Actions from '../constants/Actions';
 
+import { apiData } from '../transformers';
+
 /**
  * @extends {BaseHandler}
  */
-class GetQuestList extends BaseHandler {
+export default class GetQuestList extends BaseHandler {
   handleState() {
-    const { GET, POST } = this.method;
+    /** @type {kcsapi.api.GetQuestList.GET} */
+    const GET = this.method.GET;
 
-    let getQuest = (q) => ([q.api_no, {
-      title: q.api_title,
-      detail: q.api_detail,
-      category: q.api_category,
-      type: q.api_type,
-      reward: q.api_get_material,
-      state: q.api_state,
-      inProgress: q.api_progress_flag === 1
-    }]);
-
-    let isObject = (o) => typeof o === 'object';
-    let objects = R.filter(isObject);
-    let f = R.compose(R.fromPairs, R.map(getQuest), objects);
-    let quests = f(GET.api_list);
+    let isObject = (o) => R.is(Object, o);
+    let justObjects = R.filter(isObject);
+    let f = R.compose(R.fromPairs, R.map(apiData.transformQuest), justObjects);
+    let quests = f(GET.api_list || []);
 
     // Dispatch the action found in `BaseHandler`.
-    let action = {
+
+    this.dispatchState({
       type: Actions.UPDATE_QUEST_LIST,
       payload: {
-        quests: quests
+        quests
       }
-    };
-
-    this.dispatchState(action);
+    });
   }
 }
-
-module.exports = GetQuestList;
